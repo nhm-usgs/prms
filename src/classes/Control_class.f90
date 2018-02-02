908ecc0343cc4f93bba429e84e2984103ae53b70
Leon Foks <nfoks@usgs.gov>
2018-02-02 15:58:03 -0700

    Changed variables to array, modified constructor and read TBPs

--- src/classes/Control_class.f90
+++ src/classes/Control_class.f90
@@ -1,8 +1,9 @@
 module Control_class
     use variableKind
-    use rVariable_class, only: rVariable
-    use iVariable_class, only: iVariable
-    use sVariable_class, only: sVariable
+    use m_fileIO, only: openFile, closeFile
+    use rArray_class, only: rArray
+    use iArray_class, only: iArray
+    use sArray_class, only: sArray
     implicit none
 
     private
@@ -10,70 +11,72 @@ module Control_class
     public :: Control
 
     type Control
-        type(iVariable) :: print_debug
-        type(iVariable) :: parameter_check_flag
-        type(iVariable) :: cbh_check_flag
-        type(iVariable) :: cbh_binary_flag
+        type(iArray) :: print_debug
+        type(iArray) :: parameter_check_flag
+        type(iArray) :: cbh_check_flag
+        type(iArray) :: cbh_binary_flag
 
-        type(iVariable) :: save_vars_to_file
-        type(iVariable) :: init_vars_from_file
-        type(iVariable) :: prms_warmup
+        type(iArray) :: save_vars_to_file
+        type(iArray) :: init_vars_from_file
+        type(iArray) :: prms_warmup
 
-        type(sVariable) :: model_mode
+        type(sArray) :: model_mode
 
-        type(sVariable) :: precip_module
-        type(sVariable) :: temp_module
-        type(sVariable) :: solrad_module
-        type(sVariable) :: et_module
-        type(sVariable) :: transp_module
+        type(sArray) :: precip_module
+        type(sArray) :: temp_module
+        type(sArray) :: solrad_module
+        type(sArray) :: et_module
+        type(sArray) :: transp_module
 
-        type(sVariable) :: data_file
-        type(sVariable) :: param_file
-        type(sVariable) :: model_output_file
-        type(sVariable) :: var_save_file
-        type(sVariable) :: var_init_file
+        type(sArray) :: data_file
+        type(sArray) :: param_file
+        type(sArray) :: model_output_file
+        type(sArray) :: var_save_file
+        type(sArray) :: var_init_file
 
-        type(sVariable) :: tmax_day
-        type(sVariable) :: tmin_day
-        type(sVariable) :: precip_day
+        type(sArray) :: tmax_day
+        type(sArray) :: tmin_day
+        type(sArray) :: precip_day
 
-        type(iVariable) :: start_time
-        type(iVariable) :: end_time
+        type(iArray) :: start_time
+        type(iArray) :: end_time
 
-        type(iVariable) :: basinOutON_OFF
-        type(iVariable) :: basinOutVars
-        type(iVariable) :: basinOut_freq
-        type(sVariable) :: basinOutVar_names
-        type(sVariable) :: basinOutBaseFileName
+        type(iArray) :: basinOutON_OFF
+        type(iArray) :: basinOutVars
+        type(iArray) :: basinOut_freq
+        type(sArray) :: basinOutVar_names
+        type(sArray) :: basinOutBaseFileName
 
-        type(iVariable) :: nhruOutON_OFF
-        type(iVariable) :: nhruOutVars
-        type(iVariable) :: nhruOut_freq
-        type(sVariable) :: nhruOutVar_names
-        type(sVariable) :: nhruOutBaseFileName
+        type(iArray) :: nhruOutON_OFF
+        type(iArray) :: nhruOutVars
+        type(iArray) :: nhruOut_freq
+        type(sArray) :: nhruOutVar_names
+        type(sArray) :: nhruOutBaseFileName
 
         character(len=:), allocatable, private :: Version_read_control_file
         character(len=:), allocatable, private :: control_filename
+
+    contains
+
+        procedure, public :: read => read_Control
     end type
 
     interface Control
       !! Overloaded interface to instantiate the class.
-        module procedure :: constructor_Control
+        module function constructor_Control(control_filename) result(this)
+            type(Control) :: this
+              !! Control Class
+            character(len=*), intent(in) :: control_filename
+              !! File name to read the control parameters from.
+        end function
     end interface
 
     interface
-        module subroutine read_control_file(this)
-            type(Control), intent(inout) :: this
+        module subroutine read_Control(this, fName)
+            class(Control), intent(inout) :: this
+              !! Control Class
+            character(len=*), intent(in) :: fName
         end subroutine
     end interface
 
-contains
-    !====================================================================!
-    function constructor_Control(control_filename) result(this)
-        type(Control) :: this
-        character(len=:), allocatable, intent(in) :: control_filename
-
-        this%control_filename = control_filename
-    end function
-    !====================================================================!
 end module
