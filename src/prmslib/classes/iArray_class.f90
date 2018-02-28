3f1d5827ec5afc1a2e6174b29e7f4d17b834d555
Parker A. Norton <pnorton@usgs.gov>
2018-02-28 07:32:20 -0700

    - formatting - read_iArray()   - no longer requires filename   - changed error handling to retrieve filename from iunit

--- src/prmslib/classes/iArray_class.f90
+++ src/prmslib/classes/iArray_class.f90
@@ -1,160 +1,163 @@
 module iArray_class
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
-    !!
+  !!
+  !!
+  !!
 
-    use iso_fortran_env, only: output_unit
-    use variableKind, only: i32
-    use m_errors, only: fErr
-    use Abc_class, only: Abc
-    use m_allocate, only: allocate
-    use m_deallocate, only: deallocate
-    use m_strings, only: str
+  use iso_fortran_env, only: output_unit
+  use prms_constants, only: MAXFILE_LENGTH
+  use variableKind, only: i32
+  use m_errors, only: fErr, IO_READ
+  use Abc_class, only: Abc
+  use m_allocate, only: allocate
+  use m_deallocate, only: deallocate
+  use m_strings, only: str
 
-    implicit none
+  implicit none
 
-    private
+  private
 
-    public :: iArray
+  public :: iArray
 
-    type, extends(Abc) :: iArray
-        !! 1D array of reals that can represent multiple dimensional data
-        integer(i32), allocatable :: values(:)
-          !! The values of the array
-        integer(i32), allocatable :: dims(:)
-          !! Size(s) that values should represent. e.g. If values represents 2D data, dims might be [10, 15]
+  type, extends(Abc) :: iArray
+      !! 1D array of reals that can represent multiple dimensional data
+    integer(i32), allocatable :: values(:)
+      !! The values of the array
+    integer(i32), allocatable :: dims(:)
+      !! Size(s) that values should represent. e.g. If values represents 2D data, dims might be [10, 15]
 
     contains
-        generic, public :: allocate => allocate_iArray_i1_, allocate_iArray_i1D_
-          !! Allocate the memory inside the class
-        procedure, private, pass(this) :: allocate_iArray_i1_ => allocate_iArray_i1
-        procedure, private, pass(this) :: allocate_iArray_i1D_ => allocate_iArray_i1D
+      generic, public :: allocate => allocate_iArray_i1_, allocate_iArray_i1D_
+        !! Allocate the memory inside the class
+      procedure, private, pass(this) :: allocate_iArray_i1_ => allocate_iArray_i1
+      procedure, private, pass(this) :: allocate_iArray_i1D_ => allocate_iArray_i1D
 
-        procedure, public, pass(this) :: deallocate => deallocate_iArray
-          !! Deallocate the memory inside the class
-        procedure, public, pass(this) :: print => print_iArray
-          !! Print the class to the screen
-        procedure, public, pass(this) :: read => read_iArray
-          !! Read the class from file
-        procedure, public, pass(this) :: size => size_iArray
-
+      procedure, public, pass(this) :: deallocate => deallocate_iArray
+        !! Deallocate the memory inside the class
+      procedure, public, pass(this) :: print => print_iArray
+        !! Print the class to the screen
+      procedure, public, pass(this) :: read => read_iArray
+        !! Read the class from file
+      procedure, public, pass(this) :: size => size_iArray
     end type
 
-    interface iArray
+  interface iArray
       !! Overloaded interface to instantiate the class.
-        module procedure :: constructor_iArray_i1, constructor_iArray_i1D
-    end interface
+    module procedure :: constructor_iArray_i1, constructor_iArray_i1D
+  end interface
 
-contains
+  contains
     !====================================================================!
     function constructor_iArray_i1(N) result(this)
-        type(iArray) :: this
-          !! iArray class
-        integer(i32), intent(in) :: N
-          !! Size in 1D of the class
+      type(iArray) :: this
+        !! iArray class
+      integer(i32), intent(in) :: N
+        !! Size in 1D of the class
 
-        call this%allocate(N)
+      call this%allocate(N)
     end function
     !====================================================================!
+
     !====================================================================!
     function constructor_iArray_i1D(dims) result(this)
-        type(iArray) :: this
-          !! iArray class
-        integer(i32), intent(in) :: dims(:)
-          !! The shape of the iArray
+      type(iArray) :: this
+        !! iArray class
+      integer(i32), intent(in) :: dims(:)
+        !! The shape of the iArray
 
-        call this%allocate(dims)
+      call this%allocate(dims)
     end function
     !====================================================================!
 
     !====================================================================!
     subroutine allocate_iArray_i1(this, N)
-        class(iArray), intent(inout) :: this
-          !! iArray class
-        integer(i32), intent(in) :: N
-          !! Allocate the values to size N, and set the dims array to 1D of length one.
+      class(iArray), intent(inout) :: this
+        !! iArray class
+      integer(i32), intent(in) :: N
+        !! Allocate the values to size N, and set the dims array to 1D of length one.
 
-        call allocate(this%values, N)
-        call allocate(this%dims, 1)
-        this%dims(1) = N
-    end subroutine
+      call allocate(this%values, N)
+      call allocate(this%dims, 1)
+      this%dims(1) = N
+    end subroutine allocate_iArray_i1
     !====================================================================!
+
     !====================================================================!
     subroutine allocate_iArray_i1D(this, dims)
-        class(iArray), intent(inout) :: this
-          !! iArray class
-        integer(i32), intent(in) :: dims(:)
-          !! Allocate the values to the same shape defined by dims.
-        call allocate(this%values, product(dims))
-        call allocate(this%dims, size(dims))
-        this%dims = dims
-    end subroutine
-    !====================================================================!
+      class(iArray), intent(inout) :: this
+        !! iArray class
+      integer(i32), intent(in) :: dims(:)
+        !! Allocate the values to the same shape defined by dims.
 
+      call allocate(this%values, product(dims))
+      call allocate(this%dims, size(dims))
+      this%dims = dims
+    end subroutine allocate_iArray_i1D
+    !====================================================================!
 
     !====================================================================!
     subroutine deallocate_iArray(this)
-        class(iArray), intent(inout) :: this
-          !! iArray class
-        call deallocate(this%values)
-        call deallocate(this%dims)
-    end subroutine
-    !====================================================================!
+      class(iArray), intent(inout) :: this
+        !! iArray class
 
+      call deallocate(this%values)
+      call deallocate(this%dims)
+    end subroutine deallocate_iArray
+    !====================================================================!
 
     !====================================================================!
     subroutine print_iArray(this, delim)
-        class(iArray), intent(in) :: this
-          !! iArray class
-        character(len=*), intent(in), optional :: delim
-          !! Delimiter between values
+      class(iArray), intent(in) :: this
+        !! iArray class
+      character(len=*), intent(in), optional :: delim
+        !! Delimiter between values
 
-        write(output_unit, '(a)') str(this%values, delim)
-    end subroutine
+      write(output_unit, '(a)') str(this%values, delim)
+    end subroutine print_iArray
     !====================================================================!
 
     !====================================================================!
-    subroutine read_iArray(this, iUnit, fName)
-        class(iArray), intent(inout) :: this
-          !! iArray Class
-        integer(i32), intent(in) :: iUnit
-          !! Unit number to read from
-        character(len=*), intent(in) :: fName
-          !! Name of the file that was opened
+    subroutine read_iArray(this, iUnit) !, fName)
+      class(iArray), intent(inout) :: this
+        !! iArray Class
+      integer(i32), intent(in) :: iUnit
+        !! Unit number to read from
+      ! character(len=*), intent(in) :: fName
+        ! Name of the file that was opened
 
-        integer(i32) :: i, istat
-        integer(i32) :: N
+      integer(i32) :: ii  ! counter
+      integer(i32) :: istat  ! iostat result from read
+      integer(i32) :: numvals  ! Number of values to read
+      character(len=MAXFILE_LENGTH) :: filename
 
-        read(iUnit, *) N
-        read(iUnit, *)
+      ! Read number of values the parameter should have and allocate
+      read(iUnit, *) numvals
+      read(iUnit, *)
 
-        call this%allocate(N)
+      call this%allocate(numvals)
 
-        do i = 1, N
-            read(iUnit, *, iostat=istat) this%values(i)
-            call fErr(istat, fName, 2)
-        enddo
-    end subroutine
+      do ii = 1, numvals
+        read(iUnit, *, iostat=istat) this%values(ii)
+
+        if (istat /= 0) then
+          inquire(UNIT=iUnit, NAME=filename)
+          write(output_unit, *) "ERROR: Reading from file: " // trim(filename)
+          close(iUnit)
+          stop
+        endif
+        ! call fErr(istat, fName, IO_READ)
+      enddo
+    end subroutine read_iArray
     !====================================================================!
 
     !====================================================================!
     function size_iArray(this) result(res)
-      !! Get the size of the list of strings
-        class(iArray), intent(in) :: this
-          !! sArray Class
-        integer(i32) :: res
-          !! Size of the list
+        !! Get the size of the list of strings
+      class(iArray), intent(in) :: this
+        !! sArray Class
+      integer(i32) :: res
+        !! Size of the list
 
-        res = size(this%values)
-    end function
+      res = size(this%values)
+    end function size_iArray
     !====================================================================!
 end module
