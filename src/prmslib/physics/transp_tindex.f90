f26fa30b3e86ceb1f6c5e57f11eded281f4224aa
Parker A. Norton <pnorton@usgs.gov>
2018-04-04 11:01:14 -0600

    Added arguments to procedure calls.

--- src/prmslib/physics/transp_tindex.f90
+++ src/prmslib/physics/transp_tindex.f90
@@ -4,6 +4,7 @@
 !***********************************************************************
 MODULE PRMS_TRANSP_TINDEX
   use variableKind
+  use prms_constants, only: YEAR, MONTH, DAY
   implicit none
 
   ! Local Variables
@@ -29,9 +30,11 @@ MODULE PRMS_TRANSP_TINDEX
 
   interface Transp_tindex
     !! Transp_tindex constructor
-    module function constructor_Transp_tindex(ctl_data, param_data) result(this)
+    module function constructor_Transp_tindex(ctl_data, param_data, model_basin, climate) result(this)
       use Control_class, only: Control
       use Parameters_class, only: Parameters
+      use PRMS_BASIN, only: Basin
+      use PRMS_CLIMATEVARS, only: Climateflow
 
       type(Transp_tindex) :: this
         !! Transp_tindex class
@@ -39,29 +42,97 @@ MODULE PRMS_TRANSP_TINDEX
         !! Control file parameters
       class(Parameters), intent(in) :: param_data
         !! Parameters
+      class(Basin), intent(in) :: model_basin
+        !! Model basin information
+      class(Climateflow), intent(inout) :: climate
+        !! Climate flow class
     end function
   end interface
 
   contains
     !***********************************************************************
     ! Transp_tindex constructor
-    module function constructor_Transp_tindex(ctl_data, param_data) result(this)
+    module function constructor_Transp_tindex(ctl_data, param_data, model_basin, climate) result(this)
       use Control_class, only: Control
       use Parameters_class, only: Parameters
+      use PRMS_BASIN, only: Basin
+      use PRMS_CLIMATEVARS, only: Climateflow
+      use conversions_mod, only: c_to_f
       implicit none
 
       type(Transp_tindex) :: this
       class(Control), intent(in) :: ctl_data
       class(Parameters), intent(in) :: param_data
+      class(Basin), intent(in) :: model_basin
+      class(Climateflow), intent(inout) :: climate
+
+      integer(i32) :: ii
+        !! counter
+      integer(i32) :: chru
+        !! Current HRU
 
       ! ------------------------------------------------------------------------
+
+      ! These are only allocated when initializing from restart file
+      ! integer(i32), allocatable :: transp_beg_restart(:)
+      ! integer(i32), allocatable :: transp_end_restart(:)
+      ! real(r32), allocatable :: transp_tmax_restart(:)
+
       allocate(this%tmax_sum(ctl_data%nhru%values(1)))
       allocate(this%transp_check(ctl_data%nhru%values(1)))
       allocate(this%transp_tmax_f(ctl_data%nhru%values(1)))
+
+      if (param_data%temp_units%values(1) == 0) then
+        this%transp_tmax_f = param_data%transp_tmax%values(:)
+      else
+        do ii=1, ctl_data%nhru%values(1)
+          this%transp_tmax_f(ii) = c_to_f(param_data%transp_tmax%values(ii))
+        enddo
+      endif
+
+      ! TODO: Incorporate the load from restart file stuff
+      this%tmax_sum = 0.0
+      this%transp_check = 0
+      climate%basin_transp_on = 0
+
+      associate(st_month => ctl_data%start_time%values(MONTH), &
+                st_day => ctl_data%start_time%values(DAY), &
+                transp_beg => param_data%transp_beg%values, &
+                transp_end => param_data%transp_end%values)
+
+        do ii=1, model_basin%active_hrus
+          chru = model_basin%hru_route_order(ii)
+
+          if (st_month == transp_beg(chru)) then
+            if (st_day > 10) then
+              climate%transp_on(chru) = 1
+            else
+              this%transp_check(chru) = 1
+            endif
+          elseif (transp_end(chru) > transp_beg(chru)) then
+            if (st_month > transp_beg(chru) .and. st_month < transp_end(chru)) then
+              climate%transp_on(chru) = chru
+            endif
+          else
+            if (st_month > transp_beg(chru) .or. (st_month + 12) < (transp_end(chru) + 12)) then
+              ! TODO: shouldn't the 2nd line of the conditional just be:
+              !       st_month < transp_end(chr)
+              climate%transp_on(chru) = 1
+            endif
+          endif
+
+          if (climate%basin_transp_on == 0) then
+            if (climate%transp_on(chru) == 1) then
+              climate%basin_transp_on = 1
+            endif
+          endif
+        enddo
+
+      end associate
     end function
 
 
-    subroutine run_Transp_tindex(this, ctl_data, model_time, model_basin, param_data, climate)
+    subroutine run_Transp_tindex(this, ctl_data, param_data, model_time, model_basin, climate)
       use UTILS_PRMS, only: read_error
       use conversions_mod, only: c_to_f
 
@@ -75,9 +146,9 @@ MODULE PRMS_TRANSP_TINDEX
 
       class(Transp_tindex), intent(inout) :: this
       type(Control), intent(in) :: ctl_data
+      type(Parameters), intent(in) :: param_data
       type(Time), intent(in) :: model_time
       type(Basin), intent(in) :: model_basin
-      type(Parameters), intent(in) :: param_data
       type(Climateflow), intent(inout) :: climate
 
       ! Local Variables
@@ -90,46 +161,53 @@ MODULE PRMS_TRANSP_TINDEX
 
       !******Set switch for active transpiration period
       climate%basin_transp_on = 0
-      do j = 1, model_basin%active_hrus
-        chru = model_basin%hru_route_order(j)
 
-        !******check for month to turn transp_check switch on or
-        !******transpiration switch off (transp_on)
-        if (model_time%Nowday == 1) then
-          !******check for end of period
-          if (model_time%Nowmonth == param_data%transp_end%values(chru)) then
-            climate%transp_on(chru) = 0
-            this%transp_check(chru) = 0
-            this%tmax_sum(chru) = 0.0
+      associate(curr_month => model_time%Nowmonth, &
+                curr_day => model_time%Nowday, &
+                transp_beg => param_data%transp_beg%values, &
+                transp_end => param_data%transp_end%values)
+
+        do j = 1, model_basin%active_hrus
+          chru = model_basin%hru_route_order(j)
+
+          !******check for month to turn transp_check switch on or
+          !******transpiration switch off (transp_on)
+          if (curr_day == 1) then
+            !******check for end of period
+            if (curr_month == transp_end(chru)) then
+              climate%transp_on(chru) = 0
+              this%transp_check(chru) = 0
+              this%tmax_sum(chru) = 0.0
+            endif
+
+            !******check for month to turn transpiration switch (transp_check) on or off
+            if (curr_month == transp_beg(chru)) then
+              this%transp_check(chru) = 1
+              this%tmax_sum(chru) = 0.0
+            endif
           endif
 
-          !******check for month to turn transpiration switch (transp_check) on or off
-          if (model_time%Nowmonth == param_data%transp_beg%values(chru)) then
-            this%transp_check(chru) = 1
-            this%tmax_sum(chru) = 0.0
-          endif
-        endif
+          !****** If in checking period, then for each day sum the maximum
+          !****** temperature until it's greater than temperature index parameter,
+          !****** at which time, turn transpiration switch on, check switch off
+          ! freezing temperature assumed to be 32 degrees Fahrenheit
+          if (this%transp_check(chru) == 1) then
+            if (climate%tmaxf(chru) > 32.0) then
+              this%tmax_sum(chru) = this%tmax_sum(chru) + climate%tmaxf(chru)
+            endif
 
-        !****** If in checking period, then for each day sum the maximum
-        !****** temperature until it's greater than temperature index parameter,
-        !****** at which time, turn transpiration switch on, check switch off
-        ! freezing temperature assumed to be 32 degrees Fahrenheit
-        if (this%transp_check(chru) == 1) then
-          if (climate%tmaxf(chru) > 32.0) then
-            this%tmax_sum(chru) = this%tmax_sum(chru) + climate%tmaxf(chru)
+            if (this%tmax_sum(chru) > this%transp_tmax_f(chru)) then
+              climate%transp_on(chru) = 1
+              this%transp_check(chru) = 0
+              this%tmax_sum(chru) = 0.0
+            endif
           endif
 
-          if (this%tmax_sum(chru) > this%transp_tmax_f(chru)) then
-            climate%transp_on(chru) = 1
-            this%transp_check(chru) = 0
-            this%tmax_sum(chru) = 0.0
+          if (climate%basin_transp_on == 0) then
+            if (climate%transp_on(chru) == 1) climate%basin_transp_on = 1
           endif
-        endif
-
-        if (climate%basin_transp_on == 0) then
-          if (climate%transp_on(chru) == 1) climate%basin_transp_on = 1
-        endif
-      enddo
+        enddo
+      end associate
     end subroutine
 
     !***********************************************************************
