8adc1e2fef7764d6172205453bfd67747be6b1cf
Parker A. Norton <pnorton@usgs.gov>
2018-04-05 16:08:18 -0600

    Output module info; added use of associate().

--- src/prmslib/misc/basin.f90
+++ src/prmslib/misc/basin.f90
@@ -3,6 +3,8 @@
 !***********************************************************************
 module PRMS_BASIN
   use variableKind
+  use iso_fortran_env, only: output_unit
+  ! use fileio_mod, only: write_outfile
   implicit none
 
   private
@@ -10,8 +12,9 @@ module PRMS_BASIN
 
   intrinsic :: EPSILON
 
-  character(len=*), PARAMETER :: MODNAME = 'basin'
-  character(len=*), PARAMETER :: MODVERSION = 'basin.f90 2017-09-29 13:50:00Z'
+  character(len=*), parameter :: MODDESC = 'Basin Definition'
+  character(len=*), parameter :: MODNAME = 'basin'
+  character(len=*), parameter :: MODVERSION = '2017-09-29 13:50:00Z'
 
   type :: Basin
     real(r64) :: basin_area_inv
@@ -57,6 +60,7 @@ module PRMS_BASIN
     module function constructor_Basin(ctl_data, param_data) result(this)
       use Control_class, only: Control
       use Parameters_class, only: Parameters
+      use UTILS_PRMS, only: print_module_info
       implicit none
 
       type(Basin) :: this
@@ -78,98 +82,90 @@ module PRMS_BASIN
       ! TODO: Verify required parameters are allocated
       ! Hru_area, Hru_elev, Hru_lat, Hru_type, Cov_type, Covden_sum,
       ! Covden_win, Elev_units, Hru_percent_imperv
+      associate(nhru => ctl_data%nhru%values(1), &
+                print_debug => ctl_data%print_debug%values(1), &
+                model_output_unit => ctl_data%model_output_unit, &
+                hru_area => param_data%hru_area%values, &
+                hru_lat => param_data%hru_lat%values, &
+                hru_percent_imperv => param_data%hru_percent_imperv%values, &
+                hru_type => param_data%hru_type%values)
 
-      allocate(this%hru_route_order(ctl_data%nhru%values(1)))
-      allocate(this%hru_frac_perv(ctl_data%nhru%values(1)))
-      allocate(this%hru_imperv(ctl_data%nhru%values(1)))
-      allocate(this%hru_perv(ctl_data%nhru%values(1)))
-      this%hru_route_order = 0
-
-      j = 0
-
-      do chru = 1, ctl_data%nhru%values(1)
-        harea = param_data%hru_area%values(chru)
-        harea_dble = DBLE(harea)
-        this%total_area = this%total_area + harea_dble
-
-        ! TODO: This is not a good way to handle 'flexible' dimensions
-        !       I lean toward forcing all parameters to adhere to their
-        !       dimensions as defined in the TM chapter.
-        if (param_data%hru_type%size() == 1) then
-          ! defaults dimension is 'nhru' but it could be 'one'
-          if (param_data%hru_type%values(1) == 0) cycle ! inactive
-        else
-          ! The size of the array is something bigger than one
-          ! Assume it is 'nhru'
-          if (param_data%hru_type%values(chru) == 0) cycle ! inactive
+        if (print_debug > -2) then
+          ! Output module and version information
+          call print_module_info(MODNAME, MODDESC, MODVERSION)
         endif
 
-        ! ????????? need to fix for lakes with multiple HRUs and PRMS lake routing ????????
-        this%land_area = this%land_area + harea_dble ! swale or land
+        allocate(this%hru_route_order(nhru))
+        allocate(this%hru_frac_perv(nhru))
+        allocate(this%hru_imperv(nhru))
+        allocate(this%hru_perv(nhru))
+        this%hru_route_order = 0
 
-        this%basin_lat = this%basin_lat + DBLE(param_data%hru_lat%values(chru) * harea)
-        j = j + 1
-        this%hru_route_order(j) = chru
+        j = 0
 
-        this%hru_imperv(chru) = param_data%hru_percent_imperv%values(chru) * harea
-        this%hru_perv(chru) = harea - this%hru_imperv(chru)
+        do chru = 1, nhru
+          harea = hru_area(chru)
+          harea_dble = DBLE(harea)
+          this%total_area = this%total_area + harea_dble
 
-        this%hru_frac_perv(chru) = this%hru_perv(chru) / harea
-        basin_perv = basin_perv + DBLE(this%hru_perv(chru))
-        basin_imperv = basin_imperv + DBLE(this%hru_imperv(chru))
-      enddo
+          if (hru_type(chru) == 0) cycle ! inactive
 
-      this%active_hrus = j
-      this%active_area = this%land_area
+          ! ????????? need to fix for lakes with multiple HRUs and PRMS lake routing ????????
+          this%land_area = this%land_area + harea_dble ! swale or land
 
-      this%basin_area_inv = 1.0D0 / this%active_area
-      this%basin_lat = this%basin_lat * this%basin_area_inv
+          this%basin_lat = this%basin_lat + DBLE(hru_lat(chru) * harea)
+          j = j + 1
+          this%hru_route_order(j) = chru
 
-      ! Used in solrad modules to winter/summer radiation adjustment
-      if (this%basin_lat > 0.0D0) then
-        this%hemisphere = 0 ! Northern
-      else
-        this%hemisphere = 1 ! Southern
-      endif
+          this%hru_imperv(chru) = hru_percent_imperv(chru) * harea
+          this%hru_perv(chru) = harea - this%hru_imperv(chru)
 
-      basin_perv = basin_perv * this%basin_area_inv
-      basin_imperv = basin_imperv * this%basin_area_inv
+          this%hru_frac_perv(chru) = this%hru_perv(chru) / harea
+          basin_perv = basin_perv + DBLE(this%hru_perv(chru))
+          basin_imperv = basin_imperv + DBLE(this%hru_imperv(chru))
+        enddo
 
-      ! TODO: How to handle the print_debug stuff?
-      ! IF (Print_debug == 2) THEN
-      !   PRINT *, ' HRU     Area'
-      !   PRINT ('(I7, F14.5)'), (chru, param_data%hru_area(chru), chru = 1, Nhru)
-      !   PRINT *, 'Model domain area     = ', this%total_area
-      !   PRINT *, 'Active basin area     = ', this%active_area
-      !   PRINT *, 'Fraction impervious   = ', basin_imperv
-      !   PRINT *, 'Fraction pervious     = ', basin_perv
-      !   PRINT *, ' '
-      ! ENDIF
+        this%active_hrus = j
+        this%active_area = this%land_area
 
-      ! Print out start and end times
-      ! IF (Print_debug > -2) THEN
-      !   !CALL write_outfile(' Surface Water and Energy Budgets Simulated by '//PRMS_VERSION)
-      !   WRITE (Prms_output_unit, '(1X)')
-      !
-      !   WRITE (buffer, 9002) 'Start time: ', Starttime
-      !   CALL write_outfile(buffer(:31))
-      !
-      !   WRITE (buffer, 9002) 'End time:   ', Endtime
-      !   CALL write_outfile(buffer(:31))
-      !
-      !   WRITE (Prms_output_unit, '(1X)')
-      !   WRITE (buffer, 9003) 'Model domain area:   ', this%total_area, '    Active basin area:', this%active_area
-      !   CALL write_outfile(buffer)
-      !
-      !   WRITE (buffer, 9004) 'Fraction impervious:  ', basin_imperv, '    Fraction pervious: ', basin_perv
-      !   CALL write_outfile(buffer)
-      !   CALL write_outfile(' ')
-      ! ENDIF
-      !
-      ! 9002 FORMAT (A, I4.2, 2('/', I2.2), I3.2, 2(':', I2.2))
-      ! 9003 FORMAT (2(A, F13.2))
-      ! 9004 FORMAT (2(A, F12.5))
-      ! 9005 FORMAT (A, F13.2, A, F13.4)
+        this%basin_area_inv = 1.0D0 / this%active_area
+        this%basin_lat = this%basin_lat * this%basin_area_inv
+
+        ! Used in solrad modules to winter/summer radiation adjustment
+        if (this%basin_lat > 0.0D0) then
+          this%hemisphere = 0 ! Northern
+        else
+          this%hemisphere = 1 ! Southern
+        endif
+
+        basin_perv = basin_perv * this%basin_area_inv
+        basin_imperv = basin_imperv * this%basin_area_inv
+
+        if (print_debug == 2) then
+          write(output_unit, *) ' HRU     Area'
+          write(output_unit, fmt='(i7, f14.5)') (j, hru_area(j), j=1, nhru)
+          write(output_unit, 9001) 'Model domain area   =', this%total_area
+          write(output_unit, 9001) 'Active basin area   =', this%active_area
+          write(output_unit, 9001) 'Fraction impervious =', basin_imperv
+          write(output_unit, 9001) 'Fraction pervious   =', basin_perv
+          write(output_unit, *) ' '
+
+          9001 format(a, 1x, f15.5)
+        endif
+
+        if (print_debug > -2) then
+          write(model_output_unit, fmt='(1X)')
+          write(model_output_unit, 9003) 'Model domain area:   ', this%total_area, '    Active basin area:', this%active_area
+
+          write(model_output_unit, 9004) 'Fraction impervious:  ', basin_imperv, '    Fraction pervious: ', basin_perv
+          write(model_output_unit, fmt='(/)')
+
+          ! 9002 FORMAT (A, I4.2, 2('/', I2.2), I3.2, 2(':', I2.2))
+          9003 FORMAT(2(a, f13.2))
+          9004 FORMAT(2(a, f12.5))
+          ! 9005 FORMAT (A, F13.2, A, F13.4)
+        endif
+      end associate
     end function
 
     function module_name()
