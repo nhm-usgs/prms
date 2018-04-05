48e143f71067784024881b0dcf478aaf8402c195
Parker A. Norton <pnorton@usgs.gov>
2018-04-05 16:07:13 -0600

    Commented out unused code

--- src/prmslib/io/file_io.f90
+++ src/prmslib/io/file_io.f90
@@ -7,24 +7,24 @@ module fileio_mod
     !***********************************************************************
     ! write_outfile - print to model output file
     !***********************************************************************
-    SUBROUTINE write_outfile(out_string)
-        USE PRMS_MODULE, ONLY: PRMS_output_unit !, Print_debug
-        IMPLICIT NONE
-
-        ! Functions
-        INTRINSIC LEN_TRIM
-
-        ! Arguments
-        CHARACTER(LEN=*), INTENT(IN) :: out_string
-
-        !***********************************************************************
-        ! IF (Print_debug == -2) RETURN
-
-        IF (len_trim(out_string) > 0) THEN
-            WRITE(PRMS_output_unit, '(A)') out_string
-        ELSE
-            WRITE(PRMS_output_unit, '(/)')
-        ENDIF
-    END SUBROUTINE write_outfile
+    ! SUBROUTINE write_outfile(out_string)
+    !     USE PRMS_MODULE, ONLY: PRMS_output_unit !, Print_debug
+    !     IMPLICIT NONE
+    !
+    !     ! Functions
+    !     INTRINSIC LEN_TRIM
+    !
+    !     ! Arguments
+    !     CHARACTER(LEN=*), INTENT(IN) :: out_string
+    !
+    !     !***********************************************************************
+    !     ! IF (Print_debug == -2) RETURN
+    !
+    !     IF (len_trim(out_string) > 0) THEN
+    !         WRITE(PRMS_output_unit, '(A)') out_string
+    !     ELSE
+    !         WRITE(PRMS_output_unit, '(/)')
+    !     ENDIF
+    ! END SUBROUTINE write_outfile
 
 end module fileio_mod
