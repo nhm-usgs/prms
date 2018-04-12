3d97c4862d594052e727ee18c14042643f751bd8
Parker A. Norton <pnorton@usgs.gov>
2018-04-12 12:04:44 -0600

    was prms_simulation.f90

---
+++ src/prmslib/misc/Simulation_class.f90
@@ -0,0 +1,68 @@
+module Simulation_class
+  use variableKind
+  use Control_class, only: Control
+  use Parameters_class, only: Parameters
+  use PRMS_BASIN, only: Basin
+  use PRMS_CLIMATEVARS, only: Climateflow
+  use PRMS_CLIMATE_HRU, only: Climate_HRU
+  use PRMS_SOLTAB, only: Soltab
+  use PRMS_DDSOLRAD, only: Ddsolrad
+  use PRMS_TRANSP_TINDEX, only: Transp_tindex
+  use PRMS_POTET_JH, only: Potet_jh
+  use PRMS_BASIN_SUMMARY, only: Basin_summary
+  use PRMS_NHRU_SUMMARY, only: Nhru_summary
+  use PRMS_SET_TIME, only: Time_t
+  use PRMS_OBS, only: Obs
+  implicit none
+
+  private
+  public :: Simulation
+
+  type Simulation
+      type(Basin) :: model_basin
+      type(Climateflow) :: climate
+      type(Soltab) :: solt
+      type(Obs) :: model_obs
+      type(Time_t) :: model_time
+
+      type(Climate_HRU) :: climate_by_hru
+      type(Ddsolrad) :: solrad
+      type(Transp_tindex) :: transpiration
+      type(Potet_jh) :: potet
+      type(Nhru_summary) :: summary_by_hru
+      type(Basin_summary) :: summary_by_basin
+    contains
+      procedure, public :: run => run_Simulation
+      procedure, public :: cleanup => cleanup_Simulation
+  end type
+
+  interface Simulation
+    !! Simulation constructor
+    module function constructor_Simulation(ctl_data, param_data) result(this)
+      type(Simulation) :: this
+        !! Simulation class
+      type(Control), intent(in) :: ctl_data
+        !! Control file parameters
+      type(Parameters), intent(in) :: param_data
+        !! Parameters
+    end function
+  end interface
+
+  interface
+    module subroutine run_Simulation(this, ctl_data, param_data)
+      class(Simulation), intent(inout) :: this
+      type(Control), intent(in) :: ctl_data
+      type(Parameters), intent(in) :: param_data
+    end subroutine
+  end interface
+
+  interface
+    module subroutine cleanup_Simulation(this, ctl_data)
+      implicit none
+
+      class(Simulation), intent(in) :: this
+      type(Control), intent(in) :: ctl_data
+    end subroutine
+  end interface
+
+end module
