f85ad10a9574d786d2788c4f5459a91d7ab9858a
Parker A. Norton <pnorton@usgs.gov>
2018-04-18 15:27:00 -0600

    Removed unneeded constants; added sp and dp real kinds.

--- src/prmslib/misc/constants.f90
+++ src/prmslib/misc/constants.f90
@@ -3,41 +3,38 @@ module prms_constants
 
     implicit none
 
+    ! see discussion at: https://software.intel.com/en-us/forums/intel-visual-fortran-compiler-for-windows/topic/285082
+    integer, parameter :: sp = selected_real_kind(6, 37)
+      !! Define real precision and range
+    integer, parameter :: dp = selected_real_kind(15, 300)
+      !! Define double precision and range
+
     ! from prms6.f90
-    INTEGER(i32), PARAMETER :: MAXFILE_LENGTH = 256
-    integer(i32), parameter :: MAXCONTROL_LENGTH = 32
-    INTEGER(i32), PARAMETER :: MAXDIM = 500
-    CHARACTER(LEN=*), PARAMETER :: EQULS = '===================================================================='
+    integer(i32), parameter :: MAXFILE_LENGTH = 256
+    character(LEN=*), parameter :: EQULS = '===================================================================='
     character(len=*), parameter :: DIM_HEADER = '** Dimensions **'
     character(len=*), parameter :: PARAM_HEADER = '** Parameters **'
     character(len=*), parameter :: ENTRY_DELIMITER = '####'
 
-    ! from mmf_utils.f90
-    ! DANGER, DANGER, hard coded maximum number of paraemters and dimensions, DANGER, DANGER
-    ! INTEGER, PARAMETER :: MAXDIMENSIONS = 50
-    ! integer(i32), parameter :: MAXPARAMETERS = 200
-    ! integer(i32), parameter :: MAXVARIABLES = 100
+    real(r64), parameter :: SECS_PER_DAY = 86400_dp
+    real(r64), parameter :: SECS_PER_HOUR = 3600_dp
+    real(r32), parameter :: MIN_PER_HOUR = 60_sp
+    real(r32), parameter :: HOUR_PER_DAY = 24_sp
 
-    ! from read_control_file.f90
-    ! INTEGER(i32), PARAMETER :: Max_num_control_parameters = 150 ! WARNING, hard coded, DANGER, DANGER
+    real(r32), parameter :: NEARZERO = EPSILON(0.0)
+    real(r64), parameter :: DNEARZERO = EPSILON(0.0_dp)
 
-    real(r64), PARAMETER :: SECS_PER_DAY = 86400D0
-    real(r64), PARAMETER :: SECS_PER_HOUR = 3600D0
-    real(r32), PARAMETER :: MIN_PER_HOUR = 60
-    real(r32), PARAMETER :: HOUR_PER_DAY = 24
+    real(r64), parameter :: FT2_PER_ACRE = 43560.0_dp
+    real(r64), parameter :: CFS2CMS_CONV = 0.028316847_dp
 
-    REAL(r32), PARAMETER :: NEARZERO = EPSILON(0.0)
-    real(r64), PARAMETER :: FT2_PER_ACRE = 43560.0D0
-    real(r64), parameter :: DNEARZERO = EPSILON(0.0D0)
-    real(r64), PARAMETER :: CFS2CMS_CONV = 0.028316847D0
-    REAL(r32), PARAMETER :: INCH2MM = 25.4
-    real(r32), parameter :: INCH2M = 0.0254
+    real(r32), parameter :: INCH2MM = 25.4_sp
+    real(r32), parameter :: INCH2M = 0.0254_sp
 
     ! TODO: what units are MAXTEMP and MINTEMP?
     real(r32), parameter :: MAXTEMP = 200.0
     real(r32), parameter :: MINTEMP = -150.0
 
-    REAL(r32), PARAMETER :: MM2INCH = 1.0 / INCH2MM
+    real(r32), parameter :: MM2INCH = 1.0 / INCH2MM
 
     ! Frequency values
     ! Used for basinOut_freq, nhruOut_freq, and nsubOut_freq
