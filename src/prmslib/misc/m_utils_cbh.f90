8945506a0d27e75422e54223842a761c9e18effb
Parker A. Norton <pnorton@usgs.gov>
2018-09-25 07:01:57 -0600

    Added routines for netcdf cbh files.

--- src/prmslib/misc/m_utils_cbh.f90
+++ src/prmslib/misc/m_utils_cbh.f90
@@ -1,4 +1,5 @@
 module UTILS_CBH
+  use iso_fortran_env, only: output_unit
   use variableKind
   implicit none
 
@@ -174,4 +175,157 @@ module UTILS_CBH
         enddo
       endif
     end subroutine find_header_end
+
+    subroutine open_netcdf_cbh_file(iunit, varid, idx_offset, filename, param_name, start_time, end_time, model_nhru)
+      use netcdf
+      use PRMS_SET_TIME, only: compute_julday
+      implicit none
+
+      integer(i32), intent(out) :: iunit
+        !! Unit number for the opened netcdf file
+      integer(i32), intent(out) :: varid
+        !! Variable ID in the CBH netcdf file
+      integer(i32), intent(out) :: idx_offset
+        !! Offset into netcdf file to start_time for model
+      character(len=*), intent(in) :: filename
+        !! Name of netcdf file to open
+      character(len=*), intent(in) :: param_name
+        !! Name of parameter that should exist in the file
+      integer(i32), intent(in) :: start_time(6)
+        !! Start date specified by model
+      integer(i32), intent(in) :: end_time(6)
+        !! End date specified by model
+      integer(i32), intent(in) :: model_nhru
+        !! Number of HRUs specified for the model
+
+      ! Local variables
+      integer(i32) :: dimid
+        !! Temporary for dimension ids in the netcdf file
+      integer(i32) :: ii
+        !! Scratch variable
+      integer(i32) :: nhru
+        !! Number of HRUs as defined by the netcdf file
+      integer(i32) :: ntime
+        !! Number of timesteps in the netcdf file
+      integer(i32) :: units_len
+
+      integer(i32) :: base_dt(3)
+        !! Base date as read from the time.units attribute
+      real(r32), allocatable :: time_1D(:)
+        !! Array of time values
+
+      character(len=4), dimension(2) :: dimnames
+        !! Dimensions to read from the netcdf file
+      character(len=:), allocatable :: units_txt
+        !! Temporary for the units attribute
+
+      ! ------------------------------------------------------------------------
+      dimnames = (/ 'hru ', 'time' /)
+
+      ! Open netcdf file as read only
+      call check(nf90_open(filename, NF90_NOWRITE, iunit))
+
+      ! Get the size of the time and nhru dimensions
+      do ii = 1, size(dimnames)
+        call check(nf90_inq_dimid(iunit, dimnames(ii), dimid))
+
+        select case(dimnames(ii))
+          case('hru')
+            call check(nf90_inquire_dimension(iunit, dimid, len=nhru))
+          case('time')
+            call check(nf90_inquire_dimension(iunit, dimid, len=ntime))
+        end select
+      end do
+
+      if (nhru /= model_nhru) then
+        write(output_unit, *) 'ERROR: Number of HRUs in netcdf file, ', filename, 'do not match number of model defined HRUs'
+        stop
+      endif
+
+      ! Allocate the time and tmax variable
+      allocate(time_1D(ntime))
+
+      ! Get the varid of the time variable
+      call check(nf90_inq_varid(iunit, "time", varid))
+
+      ! Read the time data
+      call check(nf90_get_var(iunit, varid, time_1D))
+
+      call check(nf90_inquire_attribute(iunit, varid, 'units', len=units_len))
+      allocate(character(len=units_len) :: units_txt)
+      call check(nf90_get_att(iunit, varid, 'units', units_txt))
+
+      ! Get the base date for time.units='days since ...'
+      base_dt = units_to_date(units_txt)
+
+      ! Compute the offset to the location of the starting date for the model
+      ! NOTE: In PRMS this should be a zero-based offset so it can be used in
+      !       conjunction with the timestep which starts at 1.
+      idx_offset = compute_julday(start_time(1), start_time(2), start_time(3)) - &
+                   compute_julday(base_dt(1), base_dt(2), base_dt(3))
+
+      if (idx_offset < 0) then
+        write(output_unit, *) 'ERROR: start time occurs before first available date in file'
+        stop
+      endif
+
+      ! TODO: Add code to verify data exists through end_time
+
+      ! Get the varid of the data variable, based on its name.
+      call check(nf90_inq_varid(iunit, param_name, varid))
+
+    end subroutine
+
+
+    subroutine read_netcdf_cbh_file(iunit, varid, idx_offset, timestep, nhru, thedata)
+      use netcdf
+      use PRMS_SET_TIME, only: compute_julday
+      implicit none
+
+      integer(i32), intent(in) :: iunit
+        !! Unit number for the opened netcdf file
+      integer(i32), intent(in) :: varid
+        !! Variable ID in the CBH netcdf file
+      integer(i32), intent(in) :: idx_offset
+        !! Offset into netcdf file to start_time for model
+      integer(i32), intent(in) :: timestep
+        !! Current timestep of the model
+      integer(i32), intent(in) :: nhru
+        !! Number of HRUs in model
+      real(r32), allocatable, intent(inout) :: thedata(:)
+
+      ! ------------------------------------------------------------------------
+      ! Read a single day of data
+      call check(nf90_get_var(iunit, varid, thedata, start=[1, timestep+idx_offset], count=[nhru, 1]))
+
+    end subroutine
+
+
+    subroutine check(status)
+      use netcdf
+      implicit none
+
+      integer, intent ( in) :: status
+
+      if(status /= nf90_noerr) then
+        print *, trim(nf90_strerror(status))
+        stop "Stopped"
+      end if
+    end subroutine check
+
+
+    function units_to_date(units_txt) result(res)
+      integer(i32) :: res(3)
+      character(len=*), intent(in) :: units_txt
+
+      character(len=10) :: date_txt
+
+      ! --------------------------------------------------------------------------
+      date_txt = trim(units_txt(verify(units_txt, 'days since'):index(trim(units_txt), ' ', BACK=.true.)))
+
+      ! Convert the datetime text to integers
+      read(date_txt(1:index(date_txt, '-')-1), *) res(1)
+      read(date_txt(index(date_txt, '-')+1:index(date_txt, '-', BACK=.true.)-1), *) res(2)
+      read(date_txt(index(date_txt, '-', BACK=.true.)+1:), *) res(3)
+    end function
 end module
