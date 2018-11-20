fd31f6c1985ecfad7f2d5a723c5cbfd50cf201ed
Parker A. Norton <pnorton@usgs.gov>
2018-11-20 06:33:28 -0700

    Updates to tests

--- tests/pipestem/check.sh
+++ tests/pipestem/check.sh
@@ -7,6 +7,6 @@ CHECKDIR="output"
 for ff in ${BASELINE}/*; do
     filename=$(basename ${ff})
     # echo "---------- ${filename} ----------"
-    numdiff --separators=', \n' -a 0.001 -r 0.001 ${ff} ${CHECKDIR}/${filename}
+    numdiff -U --separators=', \n' -a 0.001 -r 0.001 ${ff} ${CHECKDIR}/${filename}
     # diff -q --ignore-all-space -s ${ff} ${CHECKDIR}/${filename}
 done
