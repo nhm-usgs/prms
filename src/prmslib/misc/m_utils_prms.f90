1eff5794db8863afd3c1d3dcf110e76be78dd2fe
Parker A. Norton <pnorton@usgs.gov>
2019-04-11 09:24:40 -0600

    Added support for dyanmic parameters

--- src/prmslib/misc/m_utils_prms.f90
+++ src/prmslib/misc/m_utils_prms.f90
@@ -2,7 +2,32 @@
 
 module UTILS_PRMS
   use variableKind
+  use Control_class, only: Control
+  use PRMS_SET_TIME, only: Time_t
   implicit none
+
+  interface update_parameter
+    module procedure update_parameter_i32, update_parameter_r32
+  end interface
+  ! interface update_parameter
+  !   module subroutine update_parameter_i32(ctl_data, model_time, dyn_output_unit, dyn_values, param_name, param)
+  !     type(Control), intent(in) :: ctl_data
+  !     type(Time_t), intent(in) :: model_time
+  !     integer(i32), intent(in) :: dyn_output_unit
+  !     integer(i32), intent(in) :: dyn_values(:)
+  !     character(len=*), intent(in) :: param_name
+  !     integer(i32), intent(inout) :: param(:)
+  !   end subroutine
+
+  !   module subroutine update_parameter_r32(ctl_data, model_time, dyn_output_unit, dyn_values, param_name, param)
+  !     type(Control), intent(in) :: ctl_data
+  !     type(Time_t), intent(in) :: model_time
+  !     integer(i32), intent(in) :: dyn_output_unit
+  !     real(r32), intent(in) :: dyn_values(:)
+  !     character(len=*), intent(in) :: param_name
+  !     real(r32), intent(inout) :: param(:)
+  !   end subroutine
+  ! end interface
 
   contains
     function get_array(array, shape_) result(aptr)
@@ -208,5 +233,296 @@ module UTILS_PRMS
       enddo
 
       res = good_unit
-   end function
+    end function
+
+
+    function get_first_time(iunit, datetime) result(res)
+      use prms_constants, only: YEAR, MONTH, DAY
+      implicit none
+
+      ! Argument
+      integer(i32) :: res(3)
+        !! Return a date array (YY, MM, DD)
+      integer(i32), intent(in) :: iunit
+      integer(i32), intent(in) :: datetime(3)
+        !! Datetime to search for
+
+      ! Local variables
+      logical :: found
+      integer(i32) :: dy
+        ! Date day read from file
+      integer(i32) :: iret
+      integer(i32) :: mo
+        ! Date month read from file
+      integer(i32) :: yr
+        ! Date year read from file
+
+      ! ---------------------------------------------------------------------
+      read(iunit, *, IOSTAT=iret) yr, mo, dy
+      if (iret /= 0) then
+        res = 0
+        return
+      end if
+
+      if (yr < datetime(YEAR)) then
+        found = .false.
+
+        do while (.not. found)
+          read(iunit, *, IOSTAT=iret) yr, mo, dy
+          if (iret /= 0) then
+            res = 0
+            return
+          end if
+
+          if (yr >= datetime(YEAR)) found = .true.
+        end do
+      end if
+
+      if (yr == datetime(YEAR)) then
+        if (mo < datetime(MONTH)) then
+          found = .false.
+
+          do while (.not. found)
+            read(iunit, *, IOSTAT=iret) yr, mo, dy
+            if (iret /= 0) then
+              res = 0
+              return
+            end if
+
+            if (mo >=datetime(MONTH) .or. yr /= datetime(YEAR)) found = .true.
+          end do
+        end if
+
+        if (yr == datetime(YEAR) .and. mo == datetime(MONTH)) then
+          if (dy < datetime(DAY)) then
+            found = .false.
+
+            do while (.not. found)
+              read(iunit, *, IOSTAT=iret) yr, mo, dy
+              if (iret /= 0) then
+                res = 0
+                return
+              end if
+
+              if (dy >= datetime(DAY)) found = .true.
+            end do
+          end if
+        end if
+      end if
+      backspace iunit
+      res = (/yr, mo, dy/)
+    end function
+
+
+    function get_next_time(iunit) result(res)
+      ! use prms_constants, only: YEAR, MONTH, DAY
+      implicit none
+
+      ! Argument
+      integer(i32) :: res(3)
+        !! Return a date array (YY, MM, DD)
+      integer(i32), intent(in) :: iunit
+
+      ! Local variables
+      logical :: found
+      integer(i32) :: dy
+        ! Date day read from file
+      integer(i32) :: iret
+      integer(i32) :: mo
+        ! Date month read from file
+      integer(i32) :: yr
+        ! Date year read from file
+      character(len=80) :: tmp
+      ! -----------------------------------------------------------------------
+      found = .false.
+
+      ! Skip any comment lines
+      do while(.not. found)
+        read(iunit, '(A)', IOSTAT=iret) tmp
+        if (iret /= 0) return
+        if (tmp(:2) /= '//') found = .true.
+      end do
+
+      read(tmp, *, IOSTAT=iret) yr, mo, dy
+      if (iret /= 0) then
+        res = 0
+        return
+      end if
+
+      res = (/yr, mo, dy/)
+      backspace iunit
+    end function
+
+
+    !***********************************************************************
+    ! Read file to line before data starts in file
+    !***********************************************************************
+    subroutine open_dyn_param_file(nhru, Iunit, Iret, Fname, Paramname)
+      use iso_fortran_env, only: output_unit
+      implicit none
+
+      ! Argument
+      integer(i32), intent(in) :: nhru
+        !! Expected number of HRUs in the CBH file
+      integer(i32), intent(out) :: Iunit
+      integer(i32), intent(out) :: Iret
+      character(len=*), intent(in) :: Fname
+      character(len=*), intent(in) :: Paramname
+
+      ! Local Variables
+      logical :: found
+      ! integer(i32) :: i
+      integer(i32) :: ios
+      ! integer(i32) :: dim
+      character(len=4) :: dum
+      ! character(len=80) :: dum2
+
+      !***********************************************************************
+      call PRMS_open_input_file(Iunit, Iret, Fname, Paramname)
+
+      if (Iret == 0) then
+        ! read to line before data starts in each file
+        found = .false.
+
+        do while (.not. found)
+          ! ASCII file
+          read(Iunit, FMT='(A4)', IOSTAT=ios) dum
+
+          if (ios /= 0) then
+            write(*, '(/,A,/,A,/,A)') 'ERROR reading file:', Fname, &
+                      'check to be sure the input file is in correct format'
+            Iret = 1
+            exit
+          elseif (dum == '####') then
+            found = .true.
+          end if
+        enddo
+      endif
+    end subroutine
+
+
+    !***********************************************************************
+    ! Updated values are read in, parameter values are are updated or old
+    !***********************************************************************
+    module subroutine update_parameter_i32(ctl_data, model_time, dyn_output_unit, dyn_values, param_name, param)
+      use prms_constants, only: YEAR, MONTH, DAY
+      ! use Control_class, only: Control
+      ! use PRMS_SET_TIME, only: Time_t
+      implicit none
+
+      ! Arguments
+      type(Control), intent(in) :: ctl_data
+      type(Time_t), intent(in) :: model_time
+      integer(i32), intent(in) :: dyn_output_unit
+      integer(i32), intent(in) :: dyn_values(:)
+      character(len=*), intent(in) :: param_name
+      integer(i32), intent(inout) :: param(:)
+
+      ! Local Variables
+      integer(i32) :: chru
+      integer(i32) :: num
+      integer(i32), allocatable :: updated_hrus(:)
+
+      !***********************************************************************
+      associate(print_debug => ctl_data%print_debug%value, &
+                curr_time => model_time%Nowtime)
+
+        allocate(updated_hrus(size(param)))
+        updated_hrus = 0
+        num = 0
+
+        do chru=1, size(param)
+          if (dyn_values(chru) >= 0.0) then
+            if (dyn_values(chru) /= param(chru)) then
+            ! if (abs(dyn_values(chru) - param(chru)) > NEARZERO) then
+              num = num + 1
+              updated_hrus(num) = chru
+              param(chru) = dyn_values(chru)
+            end if
+          end if
+        end do
+
+        if (print_debug > -2) then
+          write (dyn_output_unit, '(3A,I4,2("/",I2.2))') 'Parameter ', param_name, &
+                                                          ' update entry on: ', &
+                                                          curr_time(YEAR), &
+                                                          curr_time(MONTH), &
+                                                          curr_time(DAY)
+        end if
+
+        if (print_debug > -1) then
+          if (num > 0) then
+            write (dyn_output_unit, '(A,I5,2("/",I2.2))') 'List of updated HRUs; Date:', &
+                                                            curr_time(YEAR), &
+                                                            curr_time(MONTH), &
+                                                            curr_time(DAY)
+            write (dyn_output_unit, '(20I7)') (updated_hrus(chru), chru=1, num)
+          end if
+        endif
+
+        deallocate(updated_hrus)
+      end associate
+    end subroutine
+
+
+    module subroutine update_parameter_r32(ctl_data, model_time, dyn_output_unit, dyn_values, param_name, param)
+      use prms_constants, only: YEAR, MONTH, DAY
+      ! use Control_class, only: Control
+      ! use PRMS_SET_TIME, only: Time_t
+      implicit none
+
+      ! Arguments
+      type(Control), intent(in) :: ctl_data
+      type(Time_t), intent(in) :: model_time
+      integer(i32), intent(in) :: dyn_output_unit
+      real(r32), intent(in) :: dyn_values(:)
+      character(len=*), intent(in) :: param_name
+      real(r32), intent(inout) :: param(:)
+
+      ! Local Variables
+      integer(i32) :: chru
+      integer(i32) :: num
+      integer(i32), allocatable :: updated_hrus(:)
+
+      !***********************************************************************
+      associate(print_debug => ctl_data%print_debug%value, &
+                curr_time => model_time%Nowtime)
+
+        allocate(updated_hrus(size(param)))
+        updated_hrus = 0
+        num = 0
+
+        do chru=1, size(param)
+          if (dyn_values(chru) >= 0.0) then
+            if (dyn_values(chru) /= param(chru)) then
+            ! if (abs(dyn_values(chru) - param(chru)) > NEARZERO) then
+              num = num + 1
+              updated_hrus(num) = chru
+              param(chru) = dyn_values(chru)
+            end if
+          end if
+        end do
+
+        if (print_debug > -2) then
+          write (dyn_output_unit, '(3A,I4,2("/",I2.2))') 'Parameter ', param_name, &
+                                                          ' update entry on: ', &
+                                                          curr_time(YEAR), &
+                                                          curr_time(MONTH), &
+                                                          curr_time(DAY)
+        end if
+
+        if (print_debug > -1) then
+          if (num > 0) then
+            write (dyn_output_unit, '(A,I5,2("/",I2.2))') 'List of updated HRUs; Date:', &
+                                                            curr_time(YEAR), &
+                                                            curr_time(MONTH), &
+                                                            curr_time(DAY)
+            write (dyn_output_unit, '(20I7)') (updated_hrus(chru), chru=1, num)
+          end if
+        endif
+
+        deallocate(updated_hrus)
+      end associate
+    end subroutine
+
 end module
