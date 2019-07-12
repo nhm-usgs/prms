72c809346c3f8313e74b8045351b5946cd2db2d8
ashleymedin <ashley.vanbeusekom@gmail.com>
2019-07-12 00:33:29 -0600

    Now assuming seg_length includes length of sloped line not 2-D length

--- prms/routingRip.f90
+++ prms/routingRip.f90
@@ -836,7 +836,8 @@
       DO i = 1, Nsegment
         IF ( Strmflow_flag==7 ) THEN ! muskingum_mann
           velocity = (1./Mann_n(i))*SQRT(Seg_slope(i))*Seg_depth(i)**(2./3.) ! simplify if say width>>depth
-          K_coef(i) = Seg_length(i)*sqrt(1+ Seg_slope(i)**2)/(velocity*60.*60.) !want in hours
+          K_coef(i) = Seg_length(i)/(velocity*60.*60.) !want in hours, length should include sloped lenght
+          !K_coef(i) = Seg_length(i)*sqrt(1+ Seg_slope(i)**2)/(velocity*60.*60.) !want in hours
         ENDIF
 
         IF ( Segment_type(i)==2 .AND. K_coef(i)<24.0 ) K_coef(i) = 24.0 !K_coef must be specified = 24.0 for lake segments'
