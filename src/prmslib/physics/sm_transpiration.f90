de111487b532e0a027890df8788925331a32fc8c
Parker A. Norton <pnorton@usgs.gov>
2019-10-24 07:11:18 -0600

    Output summary support

--- src/prmslib/physics/sm_transpiration.f90
+++ src/prmslib/physics/sm_transpiration.f90
@@ -1,14 +1,17 @@
 submodule(PRMS_TRANSPIRATION) sm_transpiration
 contains
-  module subroutine init_Transpiration(this, ctl_data, model_basin, model_temp)
+  module subroutine init_Transpiration(this, ctl_data, model_basin, model_temp, model_summary)
     class(Transpiration), intent(inout) :: this
     type(Control), intent(in) :: ctl_data
     type(Basin), intent(in) :: model_basin
     class(Temperature), intent(in) :: model_temp
+    type(Summary), intent(inout) :: model_summary
 
     ! --------------------------------------------------------------------------
     associate(print_debug => ctl_data%print_debug%value, &
-              nhru => model_basin%nhru)
+              nhru => model_basin%nhru, &
+              outVarON_OFF => ctl_data%outVarON_OFF%value, &
+              outVar_names => ctl_data%outVar_names)
 
       call this%set_module_info(name=MODNAME, desc=MODDESC, version=MODVERSION)
 
@@ -19,6 +22,18 @@ contains
 
       allocate(this%transp_on(nhru))
       this%transp_on = .false.
+
+      ! Connect summary variables that need to be output
+      if (outVarON_OFF == 1) then
+        do jj = 1, outVar_names%size()
+          select case(outVar_names%values(jj)%s)
+            case('transp_on')
+              call model_summary%set_summary_var(jj, this%transp_on)
+            case default
+              ! pass
+          end select
+        enddo
+      endif
     end associate
   end subroutine
 
