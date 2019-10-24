9cb52b3454ce2b32d8685cbc65d01d04a84b2271
Steve Regan <rsregan@usgs.gov>
2019-10-24 16:55:33 -0600

    added parameter ppt_zero_thresh that when > 0.0 set CBH and Data File precip values to 0.0 if less than this value changed version dates in obs.f90, dynamic_param_read.f90, climate_hru.f90, climateflow.f90, nhru_summary.f90, and call_modules.f90 added parameter ncol to nhru_summary.f90 that when specified > 0 will output date on separate line and then ncol values per row for the nhru number of values BUG FIX: nhru_summary.f90: yearly integer values were output incorrectly with first two values in year-month-day format along with year Added control parameter dynamic_param_log_file so that log file of dynamic parameter changes can be named by user instead of hard-coded
    Signed-off-by: Steve Regan <rsregan@usgs.gov>

--- prms/call_modules.f90
+++ prms/call_modules.f90
@@ -92,7 +92,7 @@
 
         Process_flag = 1
 
-        PRMS_versn = 'call_modules.f90 2019-09-26 18:30:00Z'
+        PRMS_versn = 'call_modules.f90 2019-10-23 15:57:00Z'
 
         IF ( check_dims()/=0 ) STOP
 
@@ -178,8 +178,7 @@
         Process_flag = 3
         IF ( Init_vars_from_file>0 ) CLOSE ( Restart_inunit )
         IF ( Save_vars_to_file==1 ) THEN
-          nc = numchars(Var_save_file)
-          CALL PRMS_open_output_file(Restart_outunit, Var_save_file(:nc), 'var_save_file', 1, iret)
+          CALL PRMS_open_output_file(Restart_outunit, Var_save_file, 'var_save_file', 1, iret)
           IF ( iret/=0 ) STOP
           CALL call_modules_restart(0)
         ENDIF
