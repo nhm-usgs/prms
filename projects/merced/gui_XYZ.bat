ab141c697f611b8a0e284f18ed57655f835864bd
Steve Regan <rsregan@usgs.gov>
2023-12-01 18:45:48 -0700

    reran test projects

--- projects/merced/gui_XYZ.bat
+++ projects/merced/gui_XYZ.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+..\..\bin\prms -C.\control\mercdIDE.control -print
 java -cp ..\..\dist\oui4.jar oui.mms.gui.Mms .\control\mercdXYZ.control
 ECHO.
 ECHO Run complete. Please press enter to continue.
