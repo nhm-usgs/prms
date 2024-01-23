5d7433069f4d818b19b635c0df34ee2146f00b8e
Steve Regan <rsregan@usgs.gov>
2024-01-22 17:04:29 -0700

    changed git hash to last commit and reran tests

--- projects/acf/paramtool.bat
+++ projects/acf/paramtool.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-..\..\bin\prms -C.\control\acf.control -print
+
 java -cp ..\..\dist\oui4.jar oui.paramtool.ParamTool .\input\acf.params .\control\acf.control.par_name
 ECHO.
 ECHO Run complete. Please press enter to continue.
